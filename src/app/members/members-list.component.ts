0f20844b4a5e5b2dc8aaf13085d75a7f2b72c967
Tlwepzar <tlwepzar@gmail.com>
2019-08-30 07:55:07 +0200

    membership system update

--- src/app/members/members-list.component.ts
+++ src/app/members/members-list.component.ts
@@ -20,7 +20,7 @@ declare let toastr
     `
 })
 export class MembersListComponent implements OnInit {
-    @Input() public members:IMember;
+    members:IMember;
 
     constructor(private memberService: MemberService, private toastr: ToastrService, private route:ActivatedRoute){
         
